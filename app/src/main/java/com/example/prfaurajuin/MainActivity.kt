bb64d00806123d08d8b0f9be076c1a478129ea7a
gganster <guilian.ganster@gmail.com>
2024-07-01 16:32:39 +0200

    correction Todolist

--- app/src/main/java/com/example/prfaurajuin/MainActivity.kt
+++ app/src/main/java/com/example/prfaurajuin/MainActivity.kt
@@ -2,94 +2,68 @@ package com.example.prfaurajuin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
-import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import android.widget.TextView
-import androidx.core.content.ContentProviderCompat.requireContext
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
+import com.google.android.material.textfield.TextInputEditText
 
-data class Task(val id: Int, val title: String)
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var titleTaskInput : TextInputEditText
+    private lateinit var submitTask : Button
+    private lateinit var taskError : TextView
+    private lateinit var taskListView : ListView
 
-    private lateinit var listView : ListView
-    private var listItems = mutableListOf<Task>(Task(1, "hello"), Task(2, "world"))
-    private lateinit var taskAdapter: ArrayAdapter<String>
+    private var taskListData = mutableListOf<Task>()
+    private var idCounter = 0
 
-    private lateinit var countText : TextView
-    private lateinit var clockText : TextView
-    private lateinit var countButton : Button
-    private lateinit var pausePlayButton : Button
+    private lateinit var taskListViewAdapter : ArrayAdapter<String>
 
-    private var isPlaying = true
-    private var count = 0
+    private fun handleSubmit() {
+        val title : String = titleTaskInput.text.toString()
 
-    private val handler = Handler(Looper.getMainLooper())
+        taskError.visibility = android.view.View.INVISIBLE
 
-    private val updateTimeRunnable: Runnable = object : Runnable {
-        override fun run() {
-            val currentTime = Calendar.getInstance().time
-            val formatter = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
-            clockText.text = formatter.format(currentTime)
-            handler.postDelayed(this, 1000)
-        }
-    }
-    private fun onIncrement(it: View) {
-        count++
-        countText.text = "$count"
-    }
-    private fun togglePlayPause(it: View) {
-        if (isPlaying) {
-            //stop
-            handler.removeCallbacks(updateTimeRunnable)
+        if (title.isNotEmpty()) {
+            val newTask : Task = Task(idCounter++, title)
+
+            taskListData.add(newTask)
+            taskListViewAdapter.clear()
+            taskListViewAdapter.addAll(taskListData.map { it.title })
+            taskListViewAdapter.notifyDataSetChanged()
+
+            titleTaskInput.setText(null)
         } else {
-            //play
-            handler.post(updateTimeRunnable)
+            taskError.visibility = android.view.View.VISIBLE
+            taskError.text = "Entrez un titre svp"
         }
-        isPlaying = !isPlaying
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        titleTaskInput = findViewById(R.id.titleTaskInput)
+        submitTask = findViewById(R.id.submitTask)
+        taskError = findViewById(R.id.taskError)
+        taskListView = findViewById(R.id.taskListView)
 
-        //---------------------------------------------//
-        listView = findViewById<ListView>(R.id.myList)
-        taskAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems.map { it.title })
-        listView.adapter = taskAdapter
+        taskListViewAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            taskListData.map { it.title }
+        )
+        taskListView.adapter = taskListViewAdapter
 
+        taskError.visibility = android.view.View.INVISIBLE
 
-        listItems.add(Task(3, "everyone"))
-        taskAdapter.clear()
-        taskAdapter.addAll(listItems.map { it.title })
-        taskAdapter.notifyDataSetChanged()
-
-        //---------------------------------------------//
-
-        countText = findViewById<TextView>(R.id.countText)
-        clockText = findViewById<TextView>(R.id.clockText)
-
-        handler.post(updateTimeRunnable)
-
-        countText.text = "count: $count"
-
-        countButton = findViewById<Button>(R.id.countButton)
-        countButton.setOnClickListener { onIncrement(it) }
-
-        pausePlayButton = findViewById(R.id.pausePlayButton)
-        pausePlayButton.setOnClickListener { togglePlayPause(it) }
+        submitTask.setOnClickListener { handleSubmit() }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        handler.removeCallbacks(updateTimeRunnable)
     }
 }
